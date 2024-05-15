3cd2e98027fdf1989142670173f158d4a1320ad7
RomaYD <74683772+RomaYD@users.noreply.github.com>
2024-05-15 19:35:25 +0300

    add_load_madel

--- for_work.py
+++ for_work.py
@@ -1,7 +1,6 @@
 import joblib
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer
+
 # Загрузка модели из файла
 loaded_model = joblib.load('my_cuisine_classifier_test3.pkl')
 
@@ -9,10 +8,8 @@ new_dish = 'паста'
 
 data = pd.read_excel('DataTable.xlsx')
 
-vectorizer = CountVectorizer()
-learning_wordtovec = vectorizer.fit_transform(data['Dish'])
-cluster_vectoriz = LabelEncoder()
-cluster = cluster_vectoriz.fit_transform(data['Claster'])
+vectorizer = joblib.load('vectorizer.joblib')
+cluster_vectoriz = joblib.load('cluster_vectoriz.joblib')
 new_dish_vectorized = vectorizer.transform([new_dish])
 prediction = loaded_model.predict(new_dish_vectorized)
 print('Кухня нового блюда:', cluster_vectoriz.inverse_transform([prediction[0]]))
